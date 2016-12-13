c9434e9d3be2712d45f0400489696aefbaf64e7b
Leif Dreizler <leifdreizler@gmail.com>
2016-12-13 15:13:19 -0800

    resources

--- Module 3/SPSE 3.8 - DNS Poisoning.py
+++ Module 3/SPSE 3.8 - DNS Poisoning.py
@@ -8,3 +8,6 @@ from scapy.all import *
 
 # https://thepacketgeek.com/scapy-p-09-scapy-and-dns/
 # https://gist.github.com/thepacketgeek/6928674
+# https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml
+# http://blog.catchpoint.com/2014/07/09/dissecting-dns-communications/
+# http://www.zytrax.com/books/dns/ch8/
