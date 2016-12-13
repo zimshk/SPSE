4e505d06a7f752b8d9091fe10c7eb520208ba0f4
Leif Dreizler <leifdreizler@gmail.com>
2016-12-13 15:12:27 -0800

    resources

---
+++ Module 3/SPSE 3.8 - DNS Poisoning.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+
+'''
+Create a DNS poisoning tool similar to Dnsspoof using scapy
+'''
+
+from scapy.all import *
+
+# https://thepacketgeek.com/scapy-p-09-scapy-and-dns/
+# https://gist.github.com/thepacketgeek/6928674
